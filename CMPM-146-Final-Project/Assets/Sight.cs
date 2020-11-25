6879f30be9fdf0e7d0ab1c85dd90d56815d5c7c1
Luc <lmilburn@ucsc.edu>
2020-11-25 14:39:50 -0800

    switched to RayCast2D for enemy detection

--- CMPM-146-Final-Project/Assets/Sight.cs
+++ CMPM-146-Final-Project/Assets/Sight.cs
@@ -5,15 +5,25 @@ using System;
 
 public class Sight : MonoBehaviour{
 	
-    public bool enemySpotted;
-	public bool enemyHeard;
+    //public bool enemySpotted;
+	//public bool enemyHeard;
+ 	
+	void FixedUpdate()
+{
+    float laserLength = 50f;
+    Vector2 startPosition = (Vector2)transform.position + new Vector2(0.5f, 0.2f);
+    int layerMask = LayerMask.GetMask("Default");
+
+    RaycastHit2D hit = Physics2D.Raycast(startPosition, Vector2.right, laserLength, layerMask, 0);
  
-    void OnTriggerEnter2D(Collider2D col){
-        if (col.gameObject.tag == "Player"){
-			Debug.Log("Touched the hitbox");
-            Application.LoadLevel(Application.loadedLevel);
-        }
-    }
+    if (hit.collider != null){
+		if(hit.collider.tag == "Player"){
+			Debug.Log("Hitting the Player");
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+    //Debug.DrawRay(startPosition, Vector2.right * laserLength, Color.red);
+}
 	
 	//KNOWN BUGS:
 	// Hitbox doesn't account for walls at the moment. 
