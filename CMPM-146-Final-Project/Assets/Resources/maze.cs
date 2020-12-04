6c3162702958275d4e161e52ad21f83297d69027
jlao3 <65569051+jlao3@users.noreply.github.com>
2020-12-03 20:09:02 -0800

    Kind of works?

---
+++ CMPM-146-Final-Project/Assets/Resources/maze.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maze : MonoBehaviour
+{
+    public GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset t1 = (TextAsset)Resources.Load("maze", typeof(TextAsset));
+        string s = t1.text;
+        int i, j;
+        s = s.Replace("\n","");
+
+        for (i = 0; i < s.Length; i++) {
+            if (s[i] == '1') {
+
+                int column, row;
+
+                column = i % 10;
+
+                row = i / 10;
+
+                GameObject t;
+
+                t = (GameObject)(Instantiate(wall, new Vector3(50 - column * 10, 1.5f, 50 - row * 10), Quaternion.identity));
+
+            }
+        }
+    }
+}
