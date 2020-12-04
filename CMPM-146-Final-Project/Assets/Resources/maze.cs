aef41be268daa686b8acc22f6f308b16e36a23ff
jlao3 <65569051+jlao3@users.noreply.github.com>
2020-12-04 10:39:13 -0800

    Menu

--- CMPM-146-Final-Project/Assets/Resources/maze.cs
+++ CMPM-146-Final-Project/Assets/Resources/maze.cs
@@ -7,6 +7,7 @@ public class maze : MonoBehaviour
     public GameObject wall;
     public GameObject floor;
     public GameObject key;
+
     // Start is called before the first frame update
     void Start()
     {
