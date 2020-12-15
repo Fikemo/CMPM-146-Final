0c3c3a7b1b041833eee777c2767163c34e8b5b85
Luc <lmilburn@ucsc.edu>
2020-12-15 12:19:42 -0800

    isIllegalMove added

--- CMPM-146-Final-Project/Assets/Pathfind.cs
+++ CMPM-146-Final-Project/Assets/Pathfind.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
 
 //This can be placed on the current Player sprite to see its functionality.
 
@@ -8,37 +11,35 @@ public class Pathfind : MonoBehaviour
 {
     public GameObject usbKey;
     public GameObject player;
+    private GameObject[] walls;
+    public List<List<float>> wallMaster;
 
-//Retrieves the Key(Clone) object almost instantly after map is generated.
+//Invoke is needed because objects don't exist before compile time (map isn't generated yet!)
 
     void Start()
     {
         Invoke("FindUsb", 0.001f);
-        /*
-        var walls = Invoke("getWalls", 0.002f);
-        Dictionary<GameObject, List<float>> wallMaster = new Dictionary<GameObject, List<float>>(); 
-        foreach (GameObject w in walls){
-            wallMaster[w] = new List<float>{ w.transform.position.x, w.transform.position.y };
-        }
+        Invoke("getWalls", 0.01f);
+        Invoke("getWallCoords", 0.1f);
 
-        foreach (KeyValuePair<GameObject, List<float>> kvp in wallMaster){
-            Debug.Log("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-        }
-        */
     }
 
+    //Retrives usbKey object
     private void FindUsb(){
         usbKey = GameObject.Find("Key(Clone)");
-        if(usbKey != null){
-            Debug.Log("Key was found");
-        }
-        else{
-            Debug.Log("Key NOT FOUND");
-        }
     }
 
-    private GameObject[] getWalls(){
-        return GameObject.FindGameObjectsWithTag("Wall(Clone)");
+    //Populates walls list with wall GameObjects
+    private void getWalls(){
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+    }
+
+    //Populates List of Lists storing coordinates of each wall location 
+    private void getWallCoords(){
+        wallMaster = new List<List<float>>();
+        foreach (GameObject w in walls){
+            wallMaster.Add(new List<float>{w.transform.position.x, w.transform.position.y});
+        }
     }
 
 //getDistance()
@@ -47,6 +48,17 @@ public class Pathfind : MonoBehaviour
         Vector2 v1 = new Vector2(source.transform.position.x, source.transform.position.y);
         Vector2 v2 = new Vector2(dest.transform.position.x, dest.transform.position.y);
         return Vector2.Distance(v1,v2);
+    }
+
+//isIllegalMove()
+//Compares adjacent coordinates to wallMaster. If any coordinates are a wall, the move is illegal. 
+    private bool isIllegalMove(float x, float y){
+        foreach(var w in wallMaster){
+            if(x == w[0] && y == w[1]){
+                return true;
+            }
+        }
+        return false;
     }
 
 //getAdjacentMoves()
@@ -67,7 +79,13 @@ public class Pathfind : MonoBehaviour
         foreach (var dir in dirs){
             adjX = source.transform.position.x + dir[0];
             adjY = source.transform.position.y + dir[1];
-            adjList.Add(new List<float>{adjX, adjY});
+            if(isIllegalMove(adjX, adjY)){
+                continue;
+            }
+            else{ 
+                adjList.Add(new List<float>{adjX, adjY}); 
+            }
+           
         }
         return adjList;
     }
@@ -78,7 +96,20 @@ public class Pathfind : MonoBehaviour
         foreach (List<float> i in x){
             Debug.Log("Adjacent x coord: " + i[0]);
             Debug.Log("Adjacent y coord: " + i[1]);
-            */
         }
+        */
+    }
 }
 
+/*                var t = Task.Run(async delegate
+              {
+                 await Task.Delay(1000);
+                 if(walls == null){ Debug.Log("NULL!"); }
+                 else{
+                     foreach(var w in wallMaster){
+                         Debug.Log(w[0]);
+                         Debug.Log(w[1]);
+                     }
+                 }
+              });
+*/
