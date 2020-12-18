f2308c2593f8125e766b02189a28ae23f409a7ab
Luc <lmilburn@ucsc.edu>
2020-12-17 18:17:58 -0800

    premade a* set up in different scene

--- CMPM-146-Final-Project/Assets/Pathfind.cs
+++ CMPM-146-Final-Project/Assets/Pathfind.cs
@@ -13,6 +13,7 @@ public class Pathfind : MonoBehaviour
     public GameObject player;
     private GameObject[] walls;
     public List<List<float>> wallMaster;
+    public List<List<float>> visited = new List<List<float>>();
 
 //Invoke is needed because objects don't exist before compile time (map isn't generated yet!)
 
@@ -28,9 +29,10 @@ public class Pathfind : MonoBehaviour
 
     IEnumerator startPathfind(){
         yield return new WaitForSeconds(5);
-        List<List<float>> x = new List<List<float>>();
+        List<float> x = new List<float>();
         x = OptimalPathFinder(player.transform.position.x, player.transform.position.y, usbKey.transform.position.x, usbKey.transform.position.y);
-        
+        visited.Add(x);
+
         if(x != null){
             Debug.Log("X list is populated");
         }
@@ -38,13 +40,23 @@ public class Pathfind : MonoBehaviour
             Debug.Log("Uh oh");
         }
 
-        foreach(List<float> i in x){
-            Vector3 coord = new Vector3(i[0], i[1], 0f);
-            Debug.Log(i[0]+" is x coordinate");
-            Debug.Log(i[1]+" is y coordinate");
-            Vector3.MoveTowards(transform.position, coord, 2f*Time.deltaTime);
-        }
+        bool destFound = false;
 
+        while(!destFound){
+            if(x[0] == usbKey.transform.position.x && x[1] == usbKey.transform.position.y){
+                destFound = true;
+            }
+            transform.position = new Vector3(x[0], x[1], 0f);
+            x = OptimalPathFinder(x[0],x[1],usbKey.transform.position.x,usbKey.transform.position.y);
+            Debug.Log(x[0]+" is x");
+            Debug.Log(x[1]+" is y");
+            if(x[0] <= 0 && x[1] <= 0){ Debug.Log("Null valu"); }
+            if(x == null){
+                visited.Clear();
+                x = OptimalPathFinder(x[0],x[1],usbKey.transform.position.x,usbKey.transform.position.y);
+            }
+            visited.Add(x);
+        }
     }
 
     //Retrives usbKey object
@@ -93,6 +105,11 @@ public class Pathfind : MonoBehaviour
                 return true;
             }
         }
+        foreach(var z in visited){
+            if(x == z[0] && y == z[1]){
+                return true;
+            }
+        }
         return false;
     }
 
@@ -102,19 +119,21 @@ public class Pathfind : MonoBehaviour
     public List<List<float>> getAdjacentMoves(float pointX, float pointY){
         var adjList = new List<List<float>>();
         var dirs = new List<List<float>>();
+        float count = 0;
 
         dirs.Add(new List<float>{ 1.6f, 0f });
         dirs.Add(new List<float>{ -1.6f, 0f });
         dirs.Add(new List<float> { 0f, 1.6f });
         dirs.Add(new List<float> { 0f, -1.6f });
 
-        float adjX;
-        float adjY;
+        float adjX = 0f;
+        float adjY = 0f;
 
         foreach (var dir in dirs){
             adjX = pointX + dir[0];
             adjY = pointY + dir[1];
             if(isIllegalMove(adjX, adjY)){
+                count++;
                 continue;
             }
             else{ 
@@ -126,106 +145,20 @@ public class Pathfind : MonoBehaviour
     }
 
 
-    public List<List<float>> OptimalPathFinder(float srcX, float srcY, float destX, float destY){
-        Debug.Log("entered program");
-
-        //HeapQ consists of:
-        //Key -> Cost so far
-        //Value -> [x,y] coordinates
-        //The initial position's cost and coordinates are put in at the start.
-        SortedList<float, List<float>> heapQ = new SortedList<float, List<float>>();
-        heapQ.Add(0f, new List<float>{ srcX, srcY });
-
-        //distances consists of:
-        //Key -> [x,y] coordinates
-        //Value -> recorded distance
-        Dictionary<List<float>, float> distances = new Dictionary<List<float>, float>();
-        distances[new List<float>{srcX, srcY}] = 0f;
-
-        //came_from consists of:
-        // Key -> coordinates of child
-        // Value -> coordinates of parent (null if no parent)
-        Dictionary<List<float>, List<float>> came_from = new Dictionary<List<float>, List<float>>();
-        came_from[new List<float> {srcX, srcY}] = null;
-
-        //Initialize variables for later usage
-        float curr_distance = 0f;
-        float adjCost = 0f;
-        float pathCost = 0f;
-        bool newAdj = true;
-        bool betterCost = false;
-
-        List<float> curr_coords = new List<float>();
-
-        //While the heapQ is not empty,
-        //Assign curr_distance to the sortedArray's first element's key,
-        //And the respective value stored. 
-        while(heapQ.Count != 0){
-            Debug.Log(heapQ.Count);
-            foreach( KeyValuePair<float, List<float>> kvp in heapQ){
-                curr_distance = kvp.Key;
-                curr_coords = kvp.Value;
-                break;
-            }
-
-            //Removes first key+ element in the list to simulate a "pop". 
-            heapQ.RemoveAt(0);
-
-            //If the current coordinates are equal to the destination coords,
-            //Add our current coords to the path list. Then, iteratively append parents until null.
-            //Reverse the list, then the coordinates should start from the beginning and proceed till the end.
-            if(curr_coords[0] == destX && curr_coords[1] == destY){
-                List<List<float>> path = new List<List<float>>();
-                path.Add(curr_coords);
-                List<float> current_parent = came_from[curr_coords];
-                while(current_parent != null){
-                    path.Add(current_parent);
-                    current_parent = came_from[current_parent];
-                }
-                path.Reverse();
-                return path;
-            }
-            //for each adjacent move from the current_coords,
-            //calculate distance to destination, and add to existing distance to get pathCost.
-            //Check to see if coords already exist, or if the current pathcost is less.
-            //If this is the case, trigger the appropriate flags.
-            //If either flag is true, update distance and parent for the adjacent move. 
-            //Finally, push the pathCost and adjMove onto the heapQ.
-            
-            foreach(List<float> adjMove in getAdjacentMoves(curr_coords[0], curr_coords[1])){
-                newAdj = true;
-                betterCost = false;
-                //double check later
-                adjCost = getDistance(adjMove[0], adjMove[1], destX, destY);
-                pathCost = curr_distance + adjCost;
-
-
-                foreach(KeyValuePair<List<float>, float> kvp in distances){
-                    if(adjMove[0] == kvp.Key[0] && adjMove[1] == kvp.Key[1]){
-                        newAdj = false;
-                        if(pathCost < kvp.Value){
-                            betterCost = true;
-                        }
-                        break;
-                    }
-                }
-                //When this line is added, things get thrown for an infinite loop + crashes Unity.
-                //Without this line, we get a key error in that there are already existing keys.
-                foreach(KeyValuePair<float, List<float>> kvp in heapQ){
-                    if(pathCost == kvp.Key){
-                        pathCost = pathCost + 0.01f;
-                    
-                }}
-                if(newAdj || betterCost){
-                    distances[adjMove] = pathCost;
-                    Debug.Log(distances[adjMove]);
-                    came_from[adjMove] = curr_coords;
-                    heapQ.Add(pathCost, adjMove);
-                }
+    public List<float> OptimalPathFinder(float srcX, float srcY, float destX, float destY){
+        Debug.Log("entered programy");
+        float x;
+        float min = 9999f;
+        List<float> bestAdj = null;
+        List<List<float>> adjMoves = getAdjacentMoves(srcX, srcY);
+        foreach(List<float> adj in adjMoves){
+            x = getDistance(adj[0],adj[1],destX,destY);
+            if(x <= min){
+                min = x;
+                bestAdj = adj; 
             }
         }
-        Debug.Log("Goodbye");
-        return null;
+        return bestAdj;
     }
         
 
