5e31da37918b2411da0e5e4aa2e423716d4746c6
Luc <lmilburn@ucsc.edu>
2020-12-18 03:51:19 -0800

    new exit texture

---
+++ CMPM-146-Final-Project/Assets/OpenExit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenExit : MonoBehaviour
+{
+
+    public Sprite exit_open;
+    public Sprite exit_door;
+    // Update is called once per frame
+    void Update()
+    {
+        if(ComputerOpenExit.haveData){
+            gameObject.GetComponent<SpriteRenderer> ().sprite = exit_open;
+        }
+        else{
+            gameObject.GetComponent<SpriteRenderer> ().sprite = exit_door;
+        }
+    }
+}
